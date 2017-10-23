7c1cf0387960de4cee0f1f3dbee5a464d7d35883
Kxoe <kxoethecat@gmail.com>
2017-10-23 16:02:19 -0600

    Created starting baseline classes for infinity potential problem.

---
+++ Infinite Potential/Conveyor.java
@@ -0,0 +1,14 @@
+
+public class Conveyor
+{
+    
+    private int[] location; 
+    
+    public Conveyor(int[] location){
+        this.location = location;
+    }
+    
+    public int[] getLocation(){
+        return this.location;
+    }
+}
---
+++ Infinite Potential/Player.java
@@ -0,0 +1,9 @@
+
+public class Player{
+    
+    private int[] location;
+    
+    public int[] getLocation(){
+        return this.location;
+    }
+}
