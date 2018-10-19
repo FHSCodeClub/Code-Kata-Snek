9127ebf16ac610870523b9f4a687fd159b1e0fb3
Saurabh Totey <SaurabhTotey@gmail.com>
2018-10-19 10:28:06 -0600

    Made basic project structure

---
+++ src/main/kotlin/com/fairviewcodeclub/snek/WebController.kt
@@ -0,0 +1,11 @@
+package com.fairviewcodeclub.snek
+
+import org.springframework.web.bind.annotation.RestController
+
+/**
+ * The class that handles managing web traffic during the competition
+ */
+@RestController
+class WebController {
+
+}
