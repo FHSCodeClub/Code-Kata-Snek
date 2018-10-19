fc4b2b24b1c17e20b3f87c5e3356c71e52fc7ffc
Saurabh Totey <SaurabhTotey@gmail.com>
2018-10-19 14:52:39 -0600

    Started making front-end display

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -1,3 +1,33 @@
-window.onload = function() {
+let screen;
+let renderer;
 
+/**
+ * Gets elements that have been loaded
+ */
+window.onload = function() {
+    screen = document.getElementById("screen");
+    renderer = screen.getContext("2d");
 };
+
+/**
+ * The function that periodically gets called in the game loop
+ * Takes in how many frames have elapsed
+ * Returns whether the next frame should be rendered or not
+ */
+function draw(frameNumber) {
+
+}
+
+/**
+ * Periodically calls the gameLoop function
+ */
+let frameCount = 0;
+let gameLoopHandler = setInterval(function () {
+    try {
+        renderer.clearRect(0, 0, screen.width, screen.height);
+        if (draw(frameCount)) {
+            clearInterval(gameLoopHandler);
+        }
+        frameCount += 1;
+    } catch (e) {}
+}, 17);
