10bba01e1fda9d650825fb7ecb589ae62d031a05
Elia Gorokhovsky <epgorokhovsky01@bvsd.org>
2018-10-21 11:38:55 -0600

    Begin working on internal logic

--- src/main/kotlin/com/fairviewcodeclub/snek/logic/World.kt
+++ src/main/kotlin/com/fairviewcodeclub/snek/logic/World.kt
@@ -6,21 +6,48 @@ enum class Player {
     RED, GREEN, BLUE, YELLOW
 }
 
-class Coordinate(val x, val y, var tile: Tile)
+enum class Direction {
+    UP, DOWN, LEFT, RIGHT
+}
 
-class World(val size) {
+class Coordinate(val x: Int, val y: Int, var tile: Tile)
 
-    tiles = Array<Tile>(this.size) { y -> Array<Coordinate>(this.size) { x -> Coordinate(x, y, NONE) } }
+class World(val size: Int) {
+
+    var tiles = Array<Array<Coordinate>>(this.size) { y -> Array<Coordinate>(this.size) { x -> Coordinate(x, y, Tile.NONE) } }
+    var waitingOn = Player.values().toMutableList()
+    var snekDirections = mutableMapOf(Player.RED to Direction.UP, Player.GREEN to Direction.DOWN, Player.BLUE to Direction.LEFT, Player.YELLOW to Direction.RIGHT)
+    var snekLocations = mutableMapOf(Player.RED to Pair(this.size - 1, 0), Player.GREEN to Pair(0, this.size - 1), Player.BLUE to Pair(this.size - 1, this.size - 1), Player.YELLOW to Pair(0, 0))
+
+    init {
+        this.tiles[0][0].tile = Tile.SNAKE
+        this.tiles[0][this.size - 1].tile = Tile.SNAKE
+        this.tiles[this.size - 1][0].tile = Tile.SNAKE
+        this.tiles[this.size - 1][this.size - 1].tile = Tile.SNAKE
+    }
 
     fun getWorldAsJson(): String {
-        return 0
+        return "0"
     }
 
     fun receivePlayerInput(player: Player, turn: Int): Boolean {
-        return false
+        if (!this.waitingOn.contains(player)) {
+            return false
+        }
+        this.waitingOn.remove(player)
+        //change snek direction
+        return true
     }
 
     fun iterateStep() {
+        /*if (!this.tiles.flatten().any { it.tile == Tile.APPLE } ) {
+            this.tiles.flatten().filter { it.tile != Tile.SNAKE }.maxBy {
+                this.snekLocations.sumBy { coord -> Math.abs(coord.second() - it.x) + Math.abs(coord.first() - it.y) }
+            }.tile = Tile.APPLE
+        }*/
+        for (i in 0 until 4) {
+            //Move sneks
+        }
     }
 
 
