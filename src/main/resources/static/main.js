c5ccbb2cddd858af42fb01af5b2aa47c8b5f8529
Saurabh Totey <SaurabhTotey@gmail.com>
2018-10-22 11:35:41 -0600

    Made frontend graphics

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -7,6 +7,19 @@ let renderer;
 window.onload = () => {
     screen = document.getElementById("screen");
     renderer = screen.getContext("2d");
+
+    function resizeProcedure() {
+        if (window.innerWidth / 35 < window.innerHeight / 35) {
+            screen.width = window.innerWidth;
+            screen.height = window.innerWidth;
+        } else {
+            screen.height = window.innerHeight;
+            screen.width = window.innerHeight;
+        }
+    }
+
+    resizeProcedure();
+    window.onresize = resizeProcedure();
 };
 
 /**
@@ -14,20 +27,40 @@ window.onload = () => {
  * Takes in how many frames have elapsed
  * Returns whether the next frame should be rendered or not
  */
-function draw(frameNumber) {
+function draw(gameState) {
+    let cellSize = screen.width / 35;
+    for (let row = 0; row < 35; row++) {
+        for (let col = 0; col < 35; col++) {
+            let tile = gameState[row][col];
+            renderer.fillStyle = "#fff";
+            if (tile === "RED_SNEK") {
+                renderer.fillStyle = "#f00";
+            } else if (tile === "BLUE_SNEK") {
+                renderer.fillStyle = "#00f";
+            } else if (tile === "GREEN_SNEK") {
+                renderer.fillStyle = "#0f0";
+            } else if (tile === "YELLOW_SNEK") {
+                renderer.fillStyle = "#ffff33";
+            } else if (tile === "APPLE") {
+                renderer.fillStyle = "#ff69b4";
+            }
+            renderer.fillRect(cellSize * row, cellSize * col, cellSize, cellSize);
+        }
+    }
+}
 
+/**
+ * Gets the game state from the API
+ */
+async function fetchGameState() {
+    return JSON.parse(await (await fetch(`${window.location.href}api`)).text());
 }
 
 /**
  * Periodically calls the gameLoop function
  */
-let frameCount = 0;
-let gameLoopHandler = setInterval(() => {
+let gameLoopHandler = setInterval(async () => {
     try {
-        renderer.clearRect(0, 0, screen.width, screen.height);
-        if (draw(frameCount)) {
-            clearInterval(gameLoopHandler);
-        }
-        frameCount += 1;
+        draw(await fetchGameState())
     } catch (e) {}
 }, 17);
